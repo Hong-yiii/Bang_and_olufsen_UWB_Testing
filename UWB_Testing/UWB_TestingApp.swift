d50110d1a8958a74fa4da5188cf2016d2b8686e9
Hong-yiii <hongyilin.mail@gmail.com>
2025-02-22 16:20:18 +0800

    initial commit

---
+++ UWB_Testing/UWB_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UWB_TestingApp.swift
+//  UWB_Testing
+//
+//  Created by Hong Yi Lin on 22/2/25.
+//
+
+import SwiftUI
+
+@main
+struct UWB_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UWB_TestingTests/UWB_TestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  UWB_TestingTests.swift
+//  UWB_TestingTests
+//
+//  Created by Hong Yi Lin on 22/2/25.
+//
+
+import Testing
+@testable import UWB_Testing
+
+struct UWB_TestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
