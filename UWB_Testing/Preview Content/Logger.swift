7487ac29b2d496aa4c1f351c15d31402f67372ba
Hong-yiii <hongyilin.mail@gmail.com>
2025-02-26 13:15:43 +0800

    Theorethically correct one of can get BLE handshake to work with airtags

--- UWB_Testing/Preview Content/Logger.swift
+++
@@ -1,9 +0,0 @@
-// Logger.swift
-import Foundation
-
-struct Logger {
-    static func log(_ message: String) {
-        let timestamp = Date().formatted()
-        print("[\(timestamp)]: \(message)")
-    }
-}
